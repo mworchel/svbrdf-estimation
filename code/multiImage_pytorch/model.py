7d8d2af3912a43778d229235b47df0c0f41ef5d3
Markus Worchel <mworchel@hotmail.de>
2019-10-28 20:01:25 +0100

    Add simple data reader and regress SVBRDF directly

--- code/multiImage_pytorch/model.py
+++ code/multiImage_pytorch/model.py
@@ -110,9 +110,9 @@ class GlobalTrackLayer(nn.Module):
 
 class Generator(nn.Module):
     input_channel_count  = 3
-    use_coords           = True
     output_channel_count = 64 
     filters_count        = 64 # "ngf" in the original code
+    use_coords           = True
 
     def __init__(self, output_channel_count, number_of_filters = 64):
         super(Generator, self).__init__()
@@ -215,21 +215,48 @@ def gamma_encode(images):
 
 writer = SummaryWriter("./logs")
 
-generator = Generator(3).cuda()
+generator = Generator(12).cuda()
 print(generator)
 
 img1 = plt.imread("./out1.png")
 img2 = plt.imread("./out2.png")
 img3 = plt.imread("./out3.png")
+
+def read_data(paths):
+    inputs  = None
+    targets = None
+    for path in paths:
+        img  = gamma_decode(torch.Tensor(plt.imread(path)).permute(2, 0, 1))
+        imgs = torch.cat(img.unsqueeze(0).split(256, dim=-1), 0)
+        input  = imgs[-5].unsqueeze(0)
+        target = torch.cat(imgs[10:].split(1, dim=0), dim=1)
+        
+        if inputs is None:
+            inputs = input
+        else:
+            inputs = torch.cat((inputs, input), dim=0)
+
+        if targets is None:
+            targets = target
+        else:
+            targets = torch.cat((targets, target), dim=0)
+
+    return inputs, targets
+
+inputs, targets = read_data(["./in1.png", "./in2.png", "./in3.png"])
+
 input = gamma_decode(torch.tensor([img1, img2]).permute(0, 3, 1, 2).cuda())
 criterion = nn.L1Loss()
-optimizer = torch.optim.Adam(generator.parameters(), lr=1e-4)
-for epoch in range(50):
+optimizer = torch.optim.Adam(generator.parameters(), lr=1e-5)
+for epoch in range(2000):
+
+    batch_inputs  = inputs[:2].cuda()
+    batch_targets = targets[:2].cuda()
 
     # in your training loop:
     optimizer.zero_grad()   # zero the gradient buffers
-    output = generator(input)
-    loss = criterion(output, input)
+    outputs = generator(batch_inputs)
+    loss = criterion(outputs, batch_targets)
     loss.backward()
     optimizer.step()    # Does the update
 
@@ -240,12 +267,27 @@ for epoch in range(50):
 writer.add_graph(generator, input)
 writer.close()
 
-plt.imshow(gamma_encode(generator(input)[0].clone().cpu().detach().permute(1, 2, 0)))
-plt.waitforbuttonpress()
+fig=plt.figure(figsize=(8, 8))
+for i in range(inputs.shape[0]):
+    input = inputs[i].unsqueeze(0).cuda()
+    #target = targets[i]
 
-plt.imshow(gamma_encode(generator(input)[1].clone().cpu().detach().permute(1, 2, 0)))
-plt.waitforbuttonpress()
+    output = generator(input)
+             
+    images = torch.cat(output.split(3, dim=1), dim=0).clone().cpu().detach().permute(0, 2, 3, 1)
 
-plt.imshow(gamma_encode(generator(gamma_decode(torch.tensor([img3]).permute(0, 3, 1, 2).cuda()))[0].clone().cpu().detach().permute(1, 2, 0)))
-plt.waitforbuttonpress()
+    fig.add_subplot(inputs.shape[0], 5, i * 5 + 1)
+    plt.imshow(gamma_encode(inputs[i].clone().cpu().detach().permute(1, 2, 0)))
 
+    fig.add_subplot(inputs.shape[0], 5, i * 5 + 2)
+    plt.imshow(gamma_encode(images[0]))
+
+    fig.add_subplot(inputs.shape[0], 5, i * 5 + 3)
+    plt.imshow(gamma_encode(images[1]))
+
+    fig.add_subplot(inputs.shape[0], 5, i * 5 + 4)
+    plt.imshow(gamma_encode(images[2]))
+
+    fig.add_subplot(inputs.shape[0], 5, i * 5 + 5)
+    plt.imshow(gamma_encode(images[3]))
+plt.show()
