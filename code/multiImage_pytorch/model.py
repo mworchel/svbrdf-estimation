40439498fad1ed877c8fe64aec613fb8ddd78fab
Markus Worchel <mworchel@hotmail.de>
2019-10-28 11:21:33 +0100

    Encapsulate encoder/decoder layers into separate class

--- code/multiImage_pytorch/model.py
+++ code/multiImage_pytorch/model.py
@@ -1,74 +1,117 @@
 import torch
 import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
 import matplotlib.pyplot as plt
+
+class EncodingLayer(nn.Module):
+    def __init__(self, input_channel_count, output_channel_count, use_instance_norm, use_activation=True):
+        super(EncodingLayer, self).__init__()
+        
+        self.input_channel_count  = input_channel_count
+        self.output_channel_count = output_channel_count
+        self.use_instance_norm    = use_instance_norm
+        self.use_activation       = use_activation
+
+        self.conv       = nn.Conv2d(input_channel_count, output_channel_count, (4, 4), stride=2, padding=(1,1))
+        self.norm       = torch.nn.InstanceNorm2d(output_channel_count, 1e-5, affine=True) if use_instance_norm else None
+        self.leaky_relu = nn.LeakyReLU(0.2) if use_activation else None
+
+    def forward(self, x):
+        if self.leaky_relu is not None:
+            x = self.leaky_relu(x)
+
+        x = self.conv(x)
+
+        if self.norm is not None:
+            x = self.norm(x)
+        
+        return x
+
+class DecodingLayer(nn.Module):
+    def __init__(self, input_channel_count, output_channel_count, use_instance_norm, use_dropout, use_activation=True):
+        super(DecodingLayer, self).__init__()
+        
+        self.input_channel_count  = input_channel_count
+        self.output_channel_count = output_channel_count
+        self.use_instance_norm    = use_instance_norm
+        self.use_dropout          = use_dropout
+        self.use_activation       = use_activation
+
+        self.deconv     = nn.ConvTranspose2d(input_channel_count, output_channel_count, (4, 4), stride=2, padding=(1, 1))
+        self.norm       = torch.nn.InstanceNorm2d(output_channel_count, 1e-5, affine=True) if use_instance_norm else None
+        self.dropout    = nn.Dropout(0.5) if use_dropout else None
+        self.leaky_relu = nn.LeakyReLU(0.2) if use_activation else None
+
+    def forward(self, x):
+        if self.leaky_relu is not None:
+            x = self.leaky_relu(x)
+
+        x = self.deconv(x)
+
+        if self.norm is not None:
+            x = self.norm(x)
+
+        if self.dropout is not None:
+            x = self.dropout(x)
+        
+        return x
+
 
 class Generator(nn.Module):
     input_channel_count  = 3
-    output_channel_count = 3 
+    output_channel_count = 64 
     filters_count        = 64 # "ngf" in the original code
-    conv_kernel_size     = (4, 4)
 
-    def __init__(self, number_of_filters, generator_output_channels):
+    def __init__(self, output_channel_count, number_of_filters = 64):
         super(Generator, self).__init__()
-        self.number_of_filters         = number_of_filters
-        self.generator_output_channels = generator_output_channels
+        self.number_of_filters    = number_of_filters
+        self.output_channel_count = output_channel_count
 
-        self.create_encoder()
-        self.create_decoder()
-    
-    def create_conv2d(self, in_channels, out_channels):
-        return nn.Conv2d(in_channels, out_channels, self.conv_kernel_size, stride=2, padding=(1,1))
+        self.create_layers()
 
-    def create_deconv2d(self, in_channels, out_channels):
-        return nn.ConvTranspose2d(in_channels, out_channels, self.conv_kernel_size, stride=2, padding=(1, 1))
-
-    def create_encoder(self):
+    def create_layers(self):
         # TODO: Handle useCoordConv
 
-        # TODO: Handle global track
+        # TODO: Handle global track   
 
-        self.leaky_relu = nn.LeakyReLU(0.2)
+        self.enc1 = EncodingLayer(self.input_channel_count,       self.number_of_filters    , False, False) # encoder_1: [batch, 256, 256, 3      ] => [batch, 128, 128, ngf    ]
+        self.enc2 = EncodingLayer(self.enc1.output_channel_count, self.number_of_filters * 2,  True)        # encoder_2: [batch, 128, 128, ngf    ] => [batch,  64,  64, ngf * 2]
+        self.enc3 = EncodingLayer(self.enc2.output_channel_count, self.number_of_filters * 4,  True)        # encoder_3: [batch,  64,  64, ngf * 2] => [batch,  32,  32, ngf * 4]
+        self.enc4 = EncodingLayer(self.enc3.output_channel_count, self.number_of_filters * 8,  True)        # encoder_4: [batch,  32,  32, ngf * 4] => [batch,  16,  16, ngf * 8]
+        self.enc5 = EncodingLayer(self.enc4.output_channel_count, self.number_of_filters * 8,  True)        # encoder_5: [batch,  16,  16, ngf * 8] => [batch,   8,   8, ngf * 8]
+        self.enc6 = EncodingLayer(self.enc5.output_channel_count, self.number_of_filters * 8,  True)        # encoder_6: [batch,   8,   8, ngf * 8] => [batch,   4,   4, ngf * 8]
+        self.enc7 = EncodingLayer(self.enc6.output_channel_count, self.number_of_filters * 8,  True)        # encoder_7: [batch,   4,   4, ngf * 8] => [batch,   2,   2, ngf * 8]
+        self.enc8 = EncodingLayer(self.enc6.output_channel_count, self.number_of_filters * 8, False)        # encoder_8: [batch,   2,   2, ngf * 8] => [batch,   1,   1, ngf * 8]                  
 
-        self.conv1 = self.create_conv2d(self.input_channel_count  , self.number_of_filters    ) # encoder_1: [batch, 256, 256, 3      ] => [batch, 128, 128, ngf    ]
-        self.conv2 = self.create_conv2d(self.number_of_filters    , self.number_of_filters * 2) # encoder_2: [batch, 128, 128, ngf    ] => [batch,  64,  64, ngf * 2]
-        self.conv3 = self.create_conv2d(self.number_of_filters * 2, self.number_of_filters * 4) # encoder_3: [batch,  64,  64, ngf * 2] => [batch,  32,  32, ngf * 4]
-        self.conv4 = self.create_conv2d(self.number_of_filters * 4, self.number_of_filters * 8) # encoder_4: [batch,  32,  32, ngf * 4] => [batch,  16,  16, ngf * 8]
-        self.conv5 = self.create_conv2d(self.number_of_filters * 8, self.number_of_filters * 8) # encoder_5: [batch,  16,  16, ngf * 8] => [batch,   8,   8, ngf * 8]
-        self.conv6 = self.create_conv2d(self.number_of_filters * 8, self.number_of_filters * 8) # encoder_6: [batch,   8,   8, ngf * 8] => [batch,   4,   4, ngf * 8]
-        self.conv7 = self.create_conv2d(self.number_of_filters * 8, self.number_of_filters * 8) # encoder_7: [batch,   4,   4, ngf * 8] => [batch,   2,   2, ngf * 8]
-        self.conv8 = self.create_conv2d(self.number_of_filters * 8, self.number_of_filters * 8) # encoder_8: [batch,   2,   2, ngf * 8] => [batch,   1,   1, ngf * 8]
-        self.conv9 = self.create_conv2d(self.number_of_filters * 8, self.number_of_filters * 8)
-
-    def create_decoder(self):
-        self.dropout = nn.Dropout(0.5)
-
-        self.deconv8 = self.create_deconv2d(self.number_of_filters * 8,     self.number_of_filters * 8) # decoder_8: [batch, 1, 1, ngf * 8] => [batch, 2, 2, ngf * 8]
-        self.deconv7 = self.create_deconv2d(2 * self.number_of_filters * 8, self.number_of_filters * 8) # decoder_7: [batch, 2, 2, 2 * ngf * 8 ] => [batch, 4, 4, ngf * 8]
-        self.deconv6 = self.create_deconv2d(2 * self.number_of_filters * 8, self.number_of_filters * 8) # decoder_6: [batch, 4, 4, 2 * ngf * 8 ] => [batch, 8, 8, ngf * 8] 
-        self.deconv5 = self.create_deconv2d(2 * self.number_of_filters * 8, self.number_of_filters * 8) # decoder_5: [batch, 8, 8, 2 * ngf * 8 ] => [batch, 16, 16, ngf * 8
-        self.deconv4 = self.create_deconv2d(2 * self.number_of_filters * 8, self.number_of_filters * 4) # decoder_4: [batch, 16, 16, 2 * ngf * 8 ] => [batch, 32, 32, ngf * 4]
-        self.deconv3 = self.create_deconv2d(2 * self.number_of_filters * 4, self.number_of_filters * 2) # decoder_3: [batch, 32, 32, ngf * 4] => [batch, 64, 64, ngf * 2]
-        self.deconv2 = self.create_deconv2d(2 * self.number_of_filters * 2, self.number_of_filters    ) # decoder_2: [batch, 64, 64, ngf * 2] => [batch, 128, 128, ngf]
-        self.deconv1 = self.create_deconv2d(2 * self.number_of_filters    , self.output_channel_count ) # decoder_2: [batch, 64, 64, ngf * 2] => [batch, 128, 128, 64]
+        self.dec8 = DecodingLayer(self.number_of_filters * 8,         self.number_of_filters * 8,  True,  True) # decoder_8: [batch,  1,  1,      ngf * 8] => [batch,   2,   2, ngf * 8]
+        self.dec7 = DecodingLayer(2 * self.dec8.output_channel_count, self.number_of_filters * 8,  True,  True) # decoder_7: [batch,  2,  2, 2 * ngf * 8 ] => [batch,   4,   4, ngf * 8]
+        self.dec6 = DecodingLayer(2 * self.dec7.output_channel_count, self.number_of_filters * 8,  True,  True) # decoder_6: [batch,  4,  4, 2 * ngf * 8 ] => [batch,   8,   8, ngf * 8] 
+        self.dec5 = DecodingLayer(2 * self.dec6.output_channel_count, self.number_of_filters * 8,  True, False) # decoder_5: [batch,  8,  8, 2 * ngf * 8 ] => [batch,  16,  16, ngf * 8]
+        self.dec4 = DecodingLayer(2 * self.dec5.output_channel_count, self.number_of_filters * 4,  True, False) # decoder_4: [batch, 16, 16, 2 * ngf * 8 ] => [batch,  32,  32, ngf * 4]
+        self.dec3 = DecodingLayer(2 * self.dec4.output_channel_count, self.number_of_filters * 2,  True, False) # decoder_3: [batch, 32, 32, 2 * ngf * 4 ] => [batch,  64,  64, ngf * 2]
+        self.dec2 = DecodingLayer(2 * self.dec3.output_channel_count, self.number_of_filters    ,  True, False) # decoder_2: [batch, 64, 64, 2 * ngf * 2 ] => [batch, 128, 128, ngf    ]
+        self.dec1 = DecodingLayer(2 * self.dec2.output_channel_count, self.output_channel_count , False, False) # decoder_1: [batch, 64, 64, 2 * ngf     ] => [batch, 128, 128, 64     ]                   
 
     def forward(self, input):
-        down1 = self.conv1(input)
-        down2 = self.conv2(self.leaky_relu(down1))
-        down3 = self.conv3(self.leaky_relu(down2))
-        down4 = self.conv4(self.leaky_relu(down3))
-        down5 = self.conv5(self.leaky_relu(down4))
-        down6 = self.conv6(self.leaky_relu(down5))
-        down7 = self.conv7(self.leaky_relu(down6))
-        down8 = self.conv8(self.leaky_relu(down7))
+        # Encoding
+        down1 = self.enc1(input)
+        down2 = self.enc2(down1)
+        down3 = self.enc3(down2)
+        down4 = self.enc4(down3)
+        down5 = self.enc5(down4)
+        down6 = self.enc6(down5)
+        down7 = self.enc7(down6)
+        down8 = self.enc8(down7)
 
-        up8 = self.dropout(self.deconv8(self.leaky_relu(down8)))
-        up7 = self.dropout(self.deconv7(self.leaky_relu(torch.cat((up8, down7), dim=1))))
-        up6 = self.dropout(self.deconv6(self.leaky_relu(torch.cat((up7, down6), dim=1))))
-        up5 =              self.deconv5(self.leaky_relu(torch.cat((up6, down5), dim=1)))
-        up4 =              self.deconv4(self.leaky_relu(torch.cat((up5, down4), dim=1)))
-        up3 =              self.deconv3(self.leaky_relu(torch.cat((up4, down3), dim=1)))
-        up2 =              self.deconv2(self.leaky_relu(torch.cat((up3, down2), dim=1)))
-        up1 =              self.deconv1(self.leaky_relu(torch.cat((up2, down1), dim=1)))
+        # Decoding
+        up8 = self.dec8(                down8)
+        up7 = self.dec7(torch.cat((up8, down7), dim=1))
+        up6 = self.dec6(torch.cat((up7, down6), dim=1))
+        up5 = self.dec5(torch.cat((up6, down5), dim=1))
+        up4 = self.dec4(torch.cat((up5, down4), dim=1))
+        up3 = self.dec3(torch.cat((up4, down3), dim=1))
+        up2 = self.dec2(torch.cat((up3, down2), dim=1))
+        up1 = self.dec1(torch.cat((up2, down1), dim=1))
 
         return up1
 
@@ -78,7 +121,9 @@ def gamma_decode(images):
 def gamma_encode(images):
     return torch.pow(images, 1.0/2.2)
 
-generator = Generator(64, 64).cuda()
+writer = SummaryWriter("./logs")
+
+generator = Generator(3).cuda()
 print(generator)
 
 img1 = plt.imread("./out1.png")
@@ -86,9 +131,8 @@ img2 = plt.imread("./out2.png")
 img3 = plt.imread("./out3.png")
 input = gamma_decode(torch.tensor([img1, img2]).permute(0, 3, 1, 2).cuda())
 criterion = nn.L1Loss()
-optimizer = torch.optim.Adam(generator.parameters(), lr=2e-4)
-
-for epoch in range(100):
+optimizer = torch.optim.Adam(generator.parameters(), lr=1e-4)
+for epoch in range(50):
 
     # in your training loop:
     optimizer.zero_grad()   # zero the gradient buffers
@@ -97,7 +141,12 @@ for epoch in range(100):
     loss.backward()
     optimizer.step()    # Does the update
 
+    writer.add_scalar("loss", loss.item(), epoch)
+
     print("Epoch {:d}, loss: {:f}".format(epoch + 1, loss.item()))
+
+writer.add_graph(generator, input)
+writer.close()
 
 plt.imshow(gamma_encode(generator(input)[0].clone().cpu().detach().permute(1, 2, 0)))
 plt.waitforbuttonpress()
@@ -105,5 +154,6 @@ plt.waitforbuttonpress()
 plt.imshow(gamma_encode(generator(input)[1].clone().cpu().detach().permute(1, 2, 0)))
 plt.waitforbuttonpress()
 
-plt.imshow(gamma_encode(generator(torch.tensor([img3]).permute(0, 3, 1, 2).cuda())[0].clone().cpu().detach().permute(1, 2, 0)))
+plt.imshow(gamma_encode(generator(gamma_decode(torch.tensor([img3]).permute(0, 3, 1, 2).cuda()))[0].clone().cpu().detach().permute(1, 2, 0)))
 plt.waitforbuttonpress()
+
