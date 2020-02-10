mworchel/svbrdf-estimation

commit 68d526c36fa5866bcb2eaf1f518e4c294c1f2b2c

Make sure the renderings in the RenderingLoss have the correct dimensions

development/multiImage_pytorch/losses.py
@@ -35,13 +35,17 @@ class RenderingLoss(nn.Module):
             scenes = env.generate_random_scenes(random_configuration_count) + env.generate_specular_scenes(specular_configuration_count)
             input_svbrdf  = input[i]
             target_svbrdf = target[i]
+            input_renderings  = []
+            target_renderings = []
             for scene in scenes:
-                batch_input_renderings.append(self.renderer.render(scene, input_svbrdf))
-                batch_target_renderings.append(self.renderer.render(scene, target_svbrdf))
+                input_renderings.append(self.renderer.render(scene, input_svbrdf))
+                target_renderings.append(self.renderer.render(scene, target_svbrdf))
+            batch_input_renderings.append(torch.cat(input_renderings, dim=0))
+            batch_target_renderings.append(torch.cat(target_renderings, dim=0))
 
         epsilon_render    = 0.1
-        input_renderings  = torch.log(torch.stack(batch_input_renderings, dim=0) + epsilon_render)
-        target_renderings = torch.log(torch.stack(batch_target_renderings, dim=0) + epsilon_render)
+        batch_input_renderings_logged  = torch.log(torch.stack(batch_input_renderings, dim=0)  + epsilon_render)
+        batch_target_renderings_logged = torch.log(torch.stack(batch_target_renderings, dim=0) + epsilon_render)
 
         loss = nn.functional.l1_loss(input_renderings, target_renderings)
 

commit 57fc8ecf5e814a15eeb7caadc1b7bdb19b2f20e1

Fix wrong L1 computation in RenderingLoss (broken since 68d526c36fa5866bcb2eaf1f518e4c294c1f2b2c)

development/multiImage_pytorch/losses.py
@@ -47,7 +47,7 @@ class RenderingLoss(nn.Module):
         batch_input_renderings_logged  = torch.log(torch.stack(batch_input_renderings, dim=0)  + epsilon_render)
         batch_target_renderings_logged = torch.log(torch.stack(batch_target_renderings, dim=0) + epsilon_render)
 
-        loss = nn.functional.l1_loss(input_renderings, target_renderings)
+        loss = nn.functional.l1_loss(batch_input_renderings_logged, batch_target_renderings_logged)
 
         return loss
 
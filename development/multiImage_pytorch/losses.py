Fix wrong L1 computation in RenderingLoss (broken since 68d526c36fa5866bcb2eaf1f518e4c294c1f2b2c)

development/multiImage_pytorch/losses.py
@@ -47,7 +47,7 @@ class RenderingLoss(nn.Module):
         batch_input_renderings_logged  = torch.log(torch.stack(batch_input_renderings, dim=0)  + epsilon_render)
         batch_target_renderings_logged = torch.log(torch.stack(batch_target_renderings, dim=0) + epsilon_render)
 
-        loss = nn.functional.l1_loss(input_renderings, target_renderings)
+        loss = nn.functional.l1_loss(batch_input_renderings_logged, batch_target_renderings_logged)
 
         return loss
 