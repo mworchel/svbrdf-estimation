55b2557f3100304b08a170669ee62a1257b95524
Markus Worchel <mworchel@hotmail.de>
2020-05-18 19:41:30 +0200

    Remove reference code to avoid potential copyright issues

--- development/multiImage_original/testModel.bat
+++
@@ -1,7 +0,0 @@
-SET outputDir="OutputDirectory"
-SET inputDir="."
-SET testFolder="testImagesExamples"
-SET checkpoint="checkpointTrained"
-
-python pixes2Material.py --mode test --output_dir %outputDir% --input_dir %inputDir% --testFolder %testFolder% --batch_size 1 --input_size 256 --nbTargets 4 --useLog --includeDiffuse --which_direction AtoB --inputMode folder --maxImages 5 --nbInputs 10 --feedMethod files --useCoordConv --checkpoint %checkpoint% --fixImageNb
-
